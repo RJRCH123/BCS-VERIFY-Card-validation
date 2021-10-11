6b792bd4e6e8a31789c34c934b1d182df006dd77
Rosamaria Rodriguez <rous9716@gmail.com>
2021-10-11 00:18:14 -0500

    Edición: Comentarios + Agregar función click para girar Tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // import validator from './validator.js';
 
 // console.log(validator);
+
+// Función para que al dar click te permita girar a la parte reversa
+const Tarjeta = document.querySelector('#Tarjeta');
+
+Tarjeta.addEventListener('click', () => {
+    Tarjeta.classList.toggle('active');
+});
