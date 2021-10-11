cb6d5f13c131aa9ec16e3823fbbd01c0111a6676
Rosamaria Rodriguez <rous9716@gmail.com>
2021-10-11 04:42:11 -0500

    Edición: Lista Select  Mes y Año

--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@
 
 // console.log(validator);
 
-// Función para girar al dar click 
+
 const   Tarjeta = document.querySelector('#Tarjeta'),
         abrirFormulario = document.querySelector('#AbrirFormulario'),
         formulario = document.querySelector('#Formulario');
 
+// Rotación de la Tarjeta
 Tarjeta.addEventListener('click', () => {
     Tarjeta.classList.toggle('active');
 });
 
+// Botón abrir Formulario
 abrirFormulario.addEventListener('click', () => {
     abrirFormulario.classList.toggle('active');
     formulario.classList.toggle('active');
 });
+
+// Lista de Selección Mes 
+for(let i = 1; i <=12; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.SelecionarMes.appendChild(opcion);
+}
+
+// Lista de Selección Año
+const AnioActual = new Date().getFullYear();
+for(let i = AnioActual; i <= AnioActual + 11; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.SelecionarAnio.appendChild(opcion);
+}
+
